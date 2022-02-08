157952bfaaf2d1a053496307ce22be76224fe9ea
emad <emad@ivas.com.eg>
2022-02-08 15:20:15 +0200

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,50 @@
+const container = document.querySelector(".container")
+const coffees = [
+    { name: "Perspiciatis", image: "images/coffee1.jpg" },
+    { name: "Voluptatem", image: "images/coffee2.jpg" },
+    { name: "Explicabo", image: "images/coffee3.jpg" },
+    { name: "Rchitecto", image: "images/coffee4.jpg" },
+    { name: " Beatae", image: "images/coffee5.jpg" },
+    { name: " Vitae", image: "images/coffee6.jpg" },
+    { name: "Inventore", image: "images/coffee7.jpg" },
+    { name: "Veritatis", image: "images/coffee8.jpg" },
+    { name: "Accusantium", image: "images/coffee9.jpg" },
+]
+
+
+
+const showCoffees = () => {
+    let output = ""
+    coffees.forEach(
+        ({ name, image }) =>
+        (output += `
+                <div class="card">
+                  <img class="card--avatar" src=${image} />
+                  <h1 class="card--title">${name}</h1>
+                  <a class="card--link" href="#">Taste</a>
+                </div>
+                `)
+    )
+    container.innerHTML = output
+}
+
+document.addEventListener("DOMContentLoaded", showCoffees)
+
+
+
+// register service worker 
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", function() {
+        navigator.serviceWorker
+            .register("/serviceWorker.js")
+            .then(res => console.log("service worker registered"))
+            .catch(err => console.log("service worker not registered", err))
+    })
+}
+
+/*
+- Here, we start by checking if the serviceWorker is supported by the current browser (as it's still not supported by all browsers).
+- after register service worker => With this update, we have now transformed our regular web app to a PWA.
+
+
+*/
---
+++ serviceWorker.js
@@ -0,0 +1,54 @@
+ // the name of the cashe
+ const staticDevCoffee = "dev-coffee-site-v1"
+
+ // the assets to store in the cache.
+ const assets = [
+     "/",
+     "/index.html",
+     "/css/style.css",
+     "/js/app.js",
+     "/images/coffee1.jpg",
+     "/images/coffee2.jpg",
+     "/images/coffee3.jpg",
+     "/images/coffee4.jpg",
+     "/images/coffee5.jpg",
+     "/images/coffee6.jpg",
+     "/images/coffee7.jpg",
+     "/images/coffee8.jpg",
+     "/images/coffee9.jpg",
+ ]
+
+
+ // add assets to cache before install 
+ self.addEventListener("install", installEvent => {
+     installEvent.waitUntil(
+         caches.open(staticDevCoffee).then(cache => {
+             cache.addAll(assets)
+         })
+     )
+ })
+
+ /*
+ - install event. It runs when a service worker is installed. It's triggered as soon as the worker executes, and it's only called once per service worker.
+ - Caching something on the browser can take some time to finish because it's asynchronous.
+So to handle it, we need to use waitUntil() which, as you might guess, waits for the action to finish.
+*/
+
+
+
+
+
+ //Fetch the assets that we cache during install 
+ self.addEventListener("fetch", fetchEvent => {
+     fetchEvent.respondWith(
+         caches.match(fetchEvent.request).then(res => {
+             return res || fetch(fetchEvent.request)
+         })
+     )
+ })
+
+ /*
+ -  we attach respondWith() to prevent the browser's default response. Instead it returns a promise because the fetch action can take time to finish.
+ - And once the cache ready, we apply the caches.match(fetchEvent.request). It will check if something in the cache matches fetchEvent.request. By the way, fetchEvent.request is just our array of assets.
+Then, it returns a promise. And finally, we can return the result if it exists or the initial fetch if not.
+ */
